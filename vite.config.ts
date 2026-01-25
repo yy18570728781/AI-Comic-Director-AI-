ffd498d11a753fc5297fcc03f6b5a24d8105f886
向晴 余友 <yy18570728781@163.com>
2026-01-26 01:09:54 +0800

    开发环境抛出访问地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [react()],
@@ -11,6 +11,7 @@ export default defineConfig({
     },
     server: {
         port: 3000,
+        host: '0.0.0.0',
         proxy: {
             '/api': {
                 target: 'http://localhost:7001',
